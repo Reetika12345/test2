cb0ae9bdda99e4c6fbf3f932406e5584311e79a4
Reetika12345 <118004957+Reetika12345@users.noreply.github.com>
2024-12-18 21:51:58 -0800

    Add files via upload

---
+++ program2.java
@@ -0,0 +1,15 @@
+public class DataTypesDemo {
+    public static void main(String[] args) {
+        int number = 25;
+        float pi = 3.14f;
+        char letter = 'A';
+        boolean flag = true;
+        String name = "Java Programming";
+
+        System.out.println("Integer: " + number);
+        System.out.println("Float: " + pi);
+        System.out.println("Character: " + letter);
+        System.out.println("Boolean: " + flag);
+        System.out.println("String: " + name);
+    }
+}
